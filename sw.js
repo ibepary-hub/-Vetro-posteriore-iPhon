7a27e77f635afc5f56f25eecdad659d54ce4dc70
ibepary-hub <nilabepary@gmail.com>
2026-09-08 22:57:39 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE="beparytech-v77";
-const RUNTIME="beparytech-v77-security-runtime";
-const SHELL=["./","./index.html","./style.css?v=77","./app.js?v=77","./manifest.json?v=77","./beparytech-logo.png","./icon-192.png","./icon-512.png","./apple-touch-icon.png","./favicon-64.png"];
+const CACHE="beparytech-v78";
+const RUNTIME="beparytech-v78-security-runtime";
+const SHELL=["./","./index.html","./style.css?v=78","./app.js?v=78","./manifest.json?v=78","./beparytech-logo.png","./icon-192.png","./icon-512.png","./apple-touch-icon.png","./favicon-64.png"];
 self.addEventListener("install",event=>{
   self.skipWaiting();
   event.waitUntil(caches.open(CACHE).then(cache=>Promise.allSettled(SHELL.map(url=>cache.add(url)))));
